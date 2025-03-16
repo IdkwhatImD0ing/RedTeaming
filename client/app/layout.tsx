6ec1a476b0d87cbf4f8793ac68cda5ced60bda36
Bill Zhang <billzhangsc@gmail.com>
2025-03-16 00:51:12 -0700

    Metadata

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,8 +4,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-import { createMetadata } from "./metadata"
-
+import { createMetadata } from './metadata'
 
 const inter = Inter({
   variable: '--font-inter',
